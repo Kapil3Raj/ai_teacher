ab14f35100f1f355f57ae8124d04953eda86e71b
Kapil3Raj <kapilthevar3@gmail.com>
2025-06-18 18:05:51 +0530

    first commit

---
+++ app/companions/new/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>new comp</div>
+  )
+}
+
+export default page
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import Navbar from '@/components/navbar'
+
+const Page = () => {
+  return (
+    <div><h1 className='text-2xl underline'>
+      hello
+      </h1>
+      <Button>
+        subject
+      </Button>
+      <Navbar></Navbar>
+      </div>
+  )
+}
+
+export default Page
